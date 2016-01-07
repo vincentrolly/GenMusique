7a98e90d845eadf78289f672813f9a407a9d5c7b
Vincent ROLLY-VACHEZ <v.rollyvachez@epsi.fr>
2016-01-07 14:59:23 +0100

    FIRST COMMIT

---
+++ Gene_Musique.BusinessLogique/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace Gene_Musique.BusinessLogique
+{
+    public class Class1
+    {
+    }
+} 

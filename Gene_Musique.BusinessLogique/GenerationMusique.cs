6c88ba4a680395776b6e7029aa114e1a82480eae
lucas DEFRANCE <lucdef69@hotmail.fr>
2016-01-14 14:54:03 +0100

    Génération initial faite

--- Gene_Musique.BusinessLogique/GenerationMusique.cs
+++ Gene_Musique.BusinessLogique/GenerationMusique.cs
@@ -10,9 +10,35 @@ namespace Gene_Musique.BusinessLogique
 {
     public class GenerationMusique
     {
-        private int[] population = new int[10];
+        public const int  NOMBRE_INDIVIDU = 10;
+        private int[] intervalNote;
+        private Individu[] population;
         private double crossover;
         private double mutation;
+        private static Random randomizer;
+        public GenerationMusique()
+        {
+            this.population = new Individu[10];
+            generationInitial();
+            
+            
+           
+        }
+        public void generationInitial()
+        {
+            int[] intervalNote = new int[2];
+            intervalNote = [30, 100];
+            int[] intervalInstrument = new int[2];
+            intervalInstrument = [0, 127];
+            for (int i=0;i<NOMBRE_INDIVIDU;i++)
+            {
+                population[i] = new Individu(intervalNote, intervalInstrument);
+            }
+        }
+        public void Accouplement()
+        {
+
+        }
 
     }
 
--- Gene_Musique.BusinessLogique/Individu.cs
+++ Gene_Musique.BusinessLogique/Individu.cs
@@ -8,9 +8,26 @@ namespace Gene_Musique.BusinessLogique
 {
     public class Individu
     {
-        private int[] notesMusique = new int[16];
+        private int[] notesMusique;
         private int typeInstrument;
         private int notation;
+        static Random randomizerNote;
+
+        /// <summary>
+        /// Constructeur permettant de générer aléatioirement un individu avec ses notes et son type
+        /// </summary>
+        /// <param name="intervalNote"></param>
+        /// <param name="intervalInstrument"></param>
+        public Individu(int [] intervalNote,int[] intervalInstrument)
+        {
+            this.notesMusique = new int[16]; ;
+            for (int i=0;i<16;i++)
+            {
+                this.notesMusique[i] = randomizerNote.Next(intervalNote[0], intervalNote[1]);
+            }
+            this.typeInstrument = randomizerNote.Next(intervalInstrument[0], intervalInstrument[1]);
+            this.notation = 5;
+        }
 
     }
 }
